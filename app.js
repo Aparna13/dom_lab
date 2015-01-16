e67ba18af1301b024cdbf63ec350738faababb59
Aparna <aparna.sainath@gmail.com>
2015-01-16 09:25:40 -0800

    submission 3

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
 // Your code here
+function start() {
+	replaceText();
+	changeBackgrdcolor();
+	getListItems();
+	panic();
+}
+
+function replaceText () {
+var title = document.getElementById("greeting");
+console.log(title.innerHTML);
+title.innerHTML = "Hello, Planet Earth!";	
+}
+
+window.onload = start
+
+
+function changeBackgrdcolor () {
+	var listItem = document.getElementsByTagName("li");
+	for (var i = 0; i < listItem.length; i++) {
+listItem[i].style.backgroundColor = "yellow";
+	}
+}
+
+
+function getListItems () {
+var listItem = document.getElementsByTagName("li");
+for (var i = 0; i< listItem.length; i++) {
+	listItem[i].addEventListener("click",strikeout);
+}
+}
+function strikeout (event) {
+	console.log(event.target);
+	event.target.setAttribute("class", "selected");
+	var pic = document.getElementsByTagName("img");
+	console.log("pic-" +  pic);
+	pic[0].setAttribute("src", "./images/"+ event.target.innerHTML + ".jpeg"); 
+}
+
+function panic () {
+	var button = document.getElementById("reset");
+	button.addEventListener("click", reset )
+
+}
+
+function reset () {
+	var listItem = document.getElementsByTagName("li");
+for (var i = 0; i< listItem.length; i++) {
+	listItem[i].setAttribute("class","");
+	var pic = document.getElementsByTagName("img");
+	pic[0].setAttribute("src", "./images/panic.jpeg");
+}
+}
